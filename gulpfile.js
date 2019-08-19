ce48a950db1c7243cf926d05da7dd31d4e0bdd17
m-schuetz <mschuetz@potree.org>
2019-08-19 16:14:12 +0200

    fixed "no such file or directory, open './build/shaders/shaders.js'"

--- gulpfile.js
+++ gulpfile.js
@@ -402,7 +402,8 @@ gulp.task("shaders", async function(){
 
 	const targetPath = `./build/shaders/shaders.js`;
 
-	fs.writeFileSync(targetPath, content);
+	fs.mkdirSync("build/shaders");
+	fs.writeFileSync(targetPath, content, {flag: "w"});
 });
 
 gulp.task('build', 
