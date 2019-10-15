6d6d4d3da23e33fdc690b9b7fd798291f2e8d05a
m-schuetz <markus_schuetz@gmx.at>
2019-10-15 13:37:29 +0200

    oriented images, highlight covered points, gradients, profile

--- src/loader/BinaryLoader.js
+++ src/loader/BinaryLoader.js
@@ -49,6 +49,7 @@ export class BinaryLoader{
 					let buffer = xhr.response;
 					this.parse(node, buffer);
 				} else {
+					//console.error(`Failed to load file! HTTP status: ${xhr.status}, file: ${url}`);
 					throw new Error(`Failed to load file! HTTP status: ${xhr.status}, file: ${url}`);
 				}
 			}
